c57b080f98b92ec4e5081fc18f89fc435126c340
Jaehoon Ahn <ahnza245@gmail.com>
2024-01-17 06:57:12 +0000

    Download 80K (10%) of models from Objaverse

--- objaverse-rendering/scripts/download.py
+++ objaverse-rendering/scripts/download.py
@@ -5,12 +5,12 @@ processes = multiprocessing.cpu_count()
 print(objaverse.__version__)
 
 uids = objaverse.load_uids()
-len(uids), type(uids)
+print(len(uids), type(uids))
 
 
 annotations = objaverse.load_annotations()
 
 objects = objaverse.load_objects(
-    uids=uids[:100],
+    uids=uids[:80000],
     download_processes=processes
 )
